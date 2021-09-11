01668a143b0319653d2a277cd54965d4b0f79ca9
Srinivas Bera <Srinivasbera@outlook.com>
2021-09-11 09:55:16 +0530

    Initial Commit

---
+++ AppInsightsLogging/AppInsights.Logging.API/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppInsights.Logging.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : Controller
+    {
+        private readonly ILogger<StudentsController> _logger;
+        
+        public StudentsController(ILogger<StudentsController> logger)
+        {
+            _logger = logger;            
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetStudent(int id)
+        {
+            //Azure portal, run query on logs and select "traces" for found all the logs based on severity
+            _logger.LogWarning($"studnet {id} not found");            
+            _logger.LogError($"No database implementation");            
+            return NoContent();
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {            
+            //Un handled exception can tracked on application insights
+            throw new ApplicationException("Error in getting student record.");
+        }
+
+        [HttpGet]
+        [Route("Results")]
+        public IActionResult GetResults([FromServices] TelemetryClient telemetryClient)
+        {            
+            //Custom tracking
+            telemetryClient.TrackMetric("Score", 70);
+            return Ok();
+        }
+    }
+}
